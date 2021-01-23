5e29f06d911d92df2e0382205fe7565d2290dbc5
Elbert Luta <elbertluta@gmail.com>
2021-01-23 20:31:46 +0800

    setup fragments with navigation component

---
+++ app/src/main/java/com/bbo/mobiledevexam/presentation/checkout/CheckoutFragment.kt
@@ -0,0 +1,25 @@
+package com.bbo.mobiledevexam.presentation.checkout
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bbo.mobiledevexam.R
+import com.bbo.mobiledevexam.databinding.FragmentCheckoutBinding
+
+
+class CheckoutFragment : Fragment() {
+
+    private lateinit var binding: FragmentCheckoutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentCheckoutBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/bbo/mobiledevexam/presentation/order/OrderFragment.kt
@@ -0,0 +1,25 @@
+package com.bbo.mobiledevexam.presentation.order
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.bbo.mobiledevexam.R
+import com.bbo.mobiledevexam.databinding.FragmentOrderBinding
+
+class OrderFragment : Fragment() {
+
+    private lateinit var binding: FragmentOrderBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = FragmentOrderBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+}
